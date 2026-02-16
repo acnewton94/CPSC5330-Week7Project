c149f261beb6ae0a411b050d613815bf449628e0
Alexander Newton <azn0100@auburn.edu>
2026-02-16 00:28:40 -0500

    this should work?

---
+++ Week 4 Project/MoodTracker/MoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoodTracker
+//
+//  Created by user290863 on 2/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Week 6 Project/CurrencyConverter/CurrencyConverter/CurrencyConverter.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyConverter.swift
+//  CurrencyConverter
+//
+//  Created by user290863 on 2/15/26.
+//
+
+import Foundation
